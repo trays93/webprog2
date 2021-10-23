2e493a698d1799ed5f0d334c949e362fe17c27de
Bálint Lovas <lovasb93@gmail.com>
2021-10-23 20:53:11 +0200

    Számstatisztika lekérdezése és megjelenítése

--- controllers/StatisticsController.php
+++ controllers/StatisticsController.php
@@ -4,6 +4,42 @@ class StatisticsController
 {
     public function indexAction()
     {
-        return new View('statistics', 'index', ['Számstatisztika']);
+        $szamstatisztika = [];
+
+        if (count($_POST) > 0) {
+            try {
+                $conn = Database::getConnection();
+
+                $stmt = $conn->prepare("SELECT COUNT(*) AS 'szam' FROM `huzott` WHERE `szam` = :szam GROUP BY `szam`");
+                $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                
+                foreach ($_POST['szamok'] as $szam) {
+                    $stmt->bindValue(':szam', $szam);
+                    $stmt->execute();
+                    $result = $stmt->fetch();
+
+                    $szamstatisztika[$szam]['hanyszor'] = $result['szam'];
+                }
+
+                $stmt = $conn->prepare("SELECT `huzas`.`ev`, `huzas`.`het` FROM `huzott` LEFT JOIN `huzas` ON `huzott`.`huzas_id` = `huzas`.`id` WHERE `szam` = :szam ORDER BY `huzott`.`id` DESC LIMIT 1");
+                $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+                foreach ($_POST['szamok'] as $szam) {
+                    $stmt->bindValue(':szam', $szam);
+                    $stmt->execute();
+                    $result = $stmt->fetch();
+
+                    $szamstatisztika[$szam]['utoljaraHuztak'] = $result['ev'] . '. ' . $result['het'] . '. hét';
+                }
+            } catch (PDOException $ex) {
+                return new View('error', 'error', [
+                    'error' => $ex->getMessage(),
+                ]);
+            }
+        }
+
+        return new View('statistics', 'index', [
+            'szamstatisztika' => $szamstatisztika,
+        ]);
     }
 }
--- views/statistics/index.php
+++ views/statistics/index.php
@@ -1,6 +1,50 @@
+<?php
+$szamstatisztika = $data['szamstatisztika'];
+?>
+
 <div class="row">
     <div class="col-12">
     <h1>Számstatisztika</h1>
-    <p class="lead">számstatisztikák</p>
+    <p class="lead">Add meg kedvenc számaidat</p>
+
+    <form method="POST">
+        <div class="row">
+            <?php for ($i = 0; $i < 6; $i++): ?>
+            <div class="col">
+                <input type="number"
+                    class="form-control"
+                    name="szamok[]"
+                    min="1"
+                    max="45" <?= !empty($_POST['szamok']) ? 'value="' . $_POST['szamok'][$i] . '"': '' ?>
+                    required>
+            </div>
+            <?php endfor; ?>
+        </div>
+        <div class="col-12 mt-3">
+            <button type="submit" class="btn btn-primary">Kérem a statisztikát</button>
+        </div>
+    </form>
+
+
+    <?php if (!empty($szamstatisztika)) : ?>
+        <table class="table mt-3">
+            <thead>
+                <tr>
+                    <th>Szám</th>
+                    <th>Hányszor húzták</th>
+                    <th>Utoljára húzták</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($szamstatisztika as $key => $value): ?>
+                <tr>
+                    <td><?= $key ?></td>
+                    <td><?= $value['hanyszor'] ?></td>
+                    <td><?= $value['utoljaraHuztak'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php endif; ?>
     </div>
 </div>
